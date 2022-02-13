4519c766c45145a1dc97f4c787d0b7a865c38438
Jodi Chui <jodychui7@gmail.com>
2022-02-12 16:40:00 -0800

    4:39 pm

--- h4hv1/ViewController.swift
+++ h4hv1/ViewController.swift
@@ -9,25 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topLabel: UILabel!
+    
+    @IBOutlet weak var secondLabel: UILabel!
+    
+    @IBOutlet weak var planNightButton: UIButton!
+    
+    @IBAction func planNight(_ sender: UIButton) {
+        // this function makes the button go to the next screen
+        topLabel.text = "Planning"
+        secondLabel.text = "Set your expected departure and arrival."
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        welcomeFunc()
+        getStartedFunc()
+        //planNightFunc()
     }
+    
+    func welcomeFunc() {
+        topLabel.text = "Welcome!"
+    }
+    
+    func getStartedFunc() {
+        secondLabel.text = "Let's get started."
+    }
+    
+    func planNightFunc() {
+        planNightButton.layer.cornerRadius = 12
 
-    func hello() {
-        print("hey")
     }
     
-    func jodi() {
-        print("heyyoo")
-    }
     
-    func HELP() {
-        print("testing one two three")
-    }
     
-    func newFunc() {
-        print("new func. yay")
-    }
+    
 }
 
