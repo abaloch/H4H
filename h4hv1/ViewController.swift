e9daf715350c3618f19c372a1273d875ab94b5f7
Jodi Chui <jodychui7@gmail.com>
2022-02-12 16:56:55 -0800

    4:56 pm

--- h4hv1/ViewController.swift
+++ h4hv1/ViewController.swift
@@ -15,14 +15,13 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var planNightButton: UIButton!
     
-    @IBAction func planNight(_ sender: UIButton) {
-        // this function makes the button go to the next screen
+    @IBAction func planNight(_ sender: UIButton) { // func. makes the button go to the next screen (setup page)
         topLabel.text = "Planning"
         secondLabel.text = "Set your expected departure and arrival."
     }
     
     
-    @IBAction func Call(_ sender: UIButton) {
+    @IBAction func Call(_ sender: UIButton) { // phone on screen after calling 911
         topLabel.text = "CALLING 911 OR ASK IF THEY WANT TO ACTUALLY CALL?"
         topLabel.textColor = .systemRed
         secondLabel.isHidden = true
@@ -30,7 +29,7 @@ class ViewController: UIViewController {
     }
     
     
-    override func viewDidLoad() { //this is like the main function
+    override func viewDidLoad() { //this is like the MAIN function
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
@@ -39,15 +38,15 @@ class ViewController: UIViewController {
         planNightFunc()
     }
     
-    func welcomeFunc() { //
+    func welcomeFunc() { // func. to say "Welcome" as first message
         topLabel.text = "Welcome!"
     }
     
-    func getStartedFunc() {
+    func getStartedFunc() { // func. to say "Let's get started" as first message
         secondLabel.text = "Let's get started."
     }
     
-    func planNightFunc() {
+    func planNightFunc() { // func to edit button in the middle of screen
         planNightButton.layer.cornerRadius = 12
 
     }
