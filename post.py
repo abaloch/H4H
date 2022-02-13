e113a11d0806399d9b58dd1e23b909de4c5fdc13
Divya Syal <divya.s@affinidi.com>
2022-02-13 08:07:17 -0800

    removing the start and end times

--- post.py
+++ post.py
@@ -17,7 +17,7 @@ def addUser():
     Name = request.args.get('Name', None)
     Ph = int(request.args.get('Ph', None))
     # '00:00:00'
-    cursor.execute(f"INSERT INTO USER VALUES ({Name},{Ph},null,null,null,null,null,0,0,0);")
+    cursor.execute(f"INSERT INTO USER VALUES ({Name},{Ph},null,null,null,0,0,0);")
     #cursor.execute(f"INSERT INTO USER VALUES (request.args.get('Name', None),request.args.get('Ph', None),null,null,"null,null,null,0,0,0)")
 
     return 'Added user!'
@@ -31,7 +31,7 @@ def setNight():
     # name = request.args.get('name', None) we have one user atm
     # phoneNumber = request.args.get('phoneNumber', None)
     phonenum = 1234567890
-    cursor.execute('''UPDATE USER SET startTime = ? WHERE Ph = ?''', (request.args.get('startTime', None), phonenum))
+    # cursor.execute('''UPDATE USER SET startTime = ? WHERE Ph = ?''', (request.args.get('startTime', None), phonenum))
 
     #cursor.execute('''UPDATE USER SET startTime = ? WHERE Ph = ?''', ('00:01:02', phonenum))
     #cursor.execute("Select * from user;")
@@ -39,7 +39,7 @@ def setNight():
     #return l
     # SET endTime = request.args.get('endTime', None) || 'new end time'
 
-    cursor.execute('''UPDATE USER SET endTime = ? WHERE Ph = ?''', (request.args.get('endTime', None), phonenum))
+    # cursor.execute('''UPDATE USER SET endTime = ? WHERE Ph = ?''', (request.args.get('endTime', None), phonenum))
     cursor.execute('''UPDATE USER SET PType = ? WHERE Ph = ?''', (request.args.get('PType', None), phonenum))
     cursor.execute('''UPDATE USER SET PValue1 = ? WHERE Ph = ?''', (request.args.get('PValue1', None), phonenum))
     cursor.execute('''UPDATE USER SET PValue2 = ? WHERE Ph = ?''', (request.args.get('PValue2', None), phonenum))
@@ -49,22 +49,6 @@ def setNight():
     return 'updated fields'
 
     # cursor.execute(f"INSERT INTO USER VALUES ({name},{phoneNumber}, {startTime}, {endTime}, {passwordType}, {pw1}, {pw2}, {closeFriend1}, {closeFriend2}, {closeFriend3})")
-
-@app.route("/updateEndTime")
-def updateEndTime():
-    connection = sqlite3.connect('h4h_database.db')
-    cursor = connection.cursor()
-    # < str: newEnd >
-    # newEnd
-    #not in SQL, have to use cursor connection to sql
-    #UPDATE USER
-    #SET endTime = newEnd
-    #WHERE PH = 1234567890
-
-    phonenum = 1234567890
-    # cursor.execute('''UPDATE USER SET endTime = ? WHERE Ph = ?''', (newEnd, phonenum))
-    cursor.execute('''UPDATE USER SET endTime = ? WHERE Ph = ?''', (request.args.get('endTime', None), phonenum))
-    return 'updated end time'
 
 if __name__ == '__post__':
     app.run(debug=True)
