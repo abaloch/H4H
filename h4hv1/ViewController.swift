e3a5edb56ff92da559f3d9316131c1b9d7493347
Asad Baloch <45577524+abaloch@users.noreply.github.com>
2022-02-13 13:07:09 -0800

    frontend api

--- h4hv1/ViewController.swift
+++ h4hv1/ViewController.swift
@@ -7,13 +7,74 @@
 
 import UIKit
 
+//
+//  api.swift
+//  h4hv1
+//
+//  Created by Asad Baloch on 2/13/22.
+//
+
+import Foundation
+
+
+struct User: Codable {
+    var Name: String? = "yo"
+    var Ph: String? = "11111111"
+//    var startTime: String? = "06:43:00"
+//    var endTime: String? =  "09:53:00"
+//    var PType: String? = "Shape"
+//    var PValue1:String? = "Horse"
+//    var PValue2: String? = "badminton"
+//    var CF1Num: String? = "111111111"
+//    var CF2Num: String? = "222222222"
+//    var CF3Num: String? = "333333333"
+}
+
+struct APIRequest {
+    
+    var resourceURL: URL
+    let urlString = "http://127.0.0.1:5000/addUser"
+   
+    init() {
+        resourceURL = URL(string: urlString)!
+    }
+    
+    //create method to get decode the json
+    func requestAPIInfo(completion: @escaping(Result<User, Error>) -> Void) {
+        
+        let dataTask = URLSession.shared.dataTask(with: resourceURL) { (data, response, error) in
+            
+            guard error == nil else {
+                print (error!.localizedDescription)
+                print ("stuck in data task")
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            
+            do {
+                let jsonData = try decoder.decode(User.self, from: data!)
+                completion(.success(jsonData))
+            }
+            catch {
+                print ("an error in catch")
+                print (error)
+            }
+            
+            
+        
+        }
+        dataTask.resume()
+    }
+}
+
 class ViewController: UIViewController {
 
-    override func viewDidLoad() { //this is like the MAIN function
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-
-    }
+      
+      override func viewDidLoad() {
+          super.viewDidLoad()
+      
+      }
     
 }
 
--- h4hv1/contacts.swift
+++ h4hv1/contacts.swift
@@ -9,12 +9,39 @@ import UIKit
 
 
 
-class contacts: UIViewController {
 
+class contacts: UIViewController {
+   
+
+    @IBOutlet weak var yourName: UITextField!
+    
+    @IBOutlet weak var yourNumber: UITextField!
+    
+    @IBOutlet weak var cf1: UITextField!
+    
+    @IBOutlet weak var cf2: UITextField!
+    
+    @IBOutlet weak var cf3: UITextField!
+    
+    
+    @IBAction func saveButton(_ sender: UIButton) {
+        let apiRequest = APIRequest()
+        let current_user = User(Name: yourName.text, Ph: yourNumber.text)
+        apiRequest.requestAPIInfo { (apiResult) in
+            print (apiResult)
+        }
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() { //this is like the MAIN function
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
+        
+        
+        
     }
     
     @IBOutlet weak var contactArrow: UIButton!
@@ -23,7 +50,7 @@ class contacts: UIViewController {
 //    
     @IBAction func toPlanningPage(_ sender: UIButton) {
         performSegue(withIdentifier: "seguetwo", sender: self) //TO MOVE TO NEXT VIEW!!!
-
+        
     }
     
     
